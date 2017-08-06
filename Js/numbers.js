255ad70ac1de188d239e9f95d16be55e527a8ab5
Hwiki09 <oubaye.rouaki@gmail.com>
2017-08-06 14:55:48 +0200

    Numbers Methods

---
+++ Js/numbers.js
@@ -0,0 +1,19 @@
+var a = -1,
+    b = 0;
+
+console.log(a/b);
+
+var num = 100;
+num.toString();
+console.log(num);
+
+var num2 = 9560;
+num2.toExponential();
+console.log(num2);
+
+var num3 = 88.7;
+num3.toFixed(0);
+console.log(num3);
+
+var num4 = parseInt("23 years");
+console.log(num4 / 2);
