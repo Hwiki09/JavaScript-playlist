41b970e44046611d7ffa108eccea39cf6a1b59d3
Hwiki09 <oubaye.rouaki@gmail.com>
2017-08-10 14:53:04 +0200

    Class List 2.0

--- Js HTML Dom/Class_List-Children.js
+++ Js HTML Dom/Class_List-Children.js
@@ -3,6 +3,9 @@
         classList.lenght
         classList.item
         classList.contains
+        classList.add
+        classList.remove
+        classList.toggle
 */
 
 var myDiv = document.getElementById('Hi');
@@ -10,3 +13,11 @@ console.log(myDiv.classList);
 console.log(myDiv.classList.contains('zero'));
 console.log(myDiv.classList.item('1'));
 console.log(myDiv.classList.length);
+
+var myButton = document.querySelector('button'),
+    myHiddenText = document.getElementById('hidden');
+
+myButton.onclick = function () {
+    myHiddenText.classList.toggle('show'); // If it Exist Delete it, Wether Create it !
+    myHiddenText.classList.add('Classs');  // Add a Class 
+}
