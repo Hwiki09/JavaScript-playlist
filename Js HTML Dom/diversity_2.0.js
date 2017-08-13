007394bc54e6a61e770b500a1eff85f6e7f1ce99
Hwiki09 <oubaye.rouaki@gmail.com>
2017-08-13 18:42:38 +0200

    inputEncoding | lastModified | URL

--- Js HTML Dom/diversity_2.0.js
+++ Js HTML Dom/diversity_2.0.js
@@ -2,7 +2,6 @@
     Style:
         Element.style.property = 'value'
 
-
 */
 
 // Style
@@ -12,6 +11,12 @@ mainDiv.style.padding = '10px';
 mainDiv.style.color = 'Black';
 mainDiv.style.fontFamily = 'Consolas';
 
+// inputEncoding | lastModified | URL
 
+console.log(document.inputEncoding);
+console.log(document.lastModified);
+console.log(document.URL);
+
+// Create: Element, textNode, Comment
 
 
