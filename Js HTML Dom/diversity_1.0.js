ba37d94b3c8f8999f6437521784d460f0d654e97
Hwiki09 <oubaye.rouaki@gmail.com>
2017-08-13 11:36:38 +0200

    Focus() / Blur()

--- Js HTML Dom/diversity_1.0.js
+++ Js HTML Dom/diversity_1.0.js
@@ -2,9 +2,19 @@
     Siblings:
         nextSiblings: Includes [Comments, Text & Elements]
         nextElementSibling: Includes [Only Elements]
+
+    Focus: Place The Cursor inside The Inserting Field
+    Blur: Place The Cursor Outside The Inserting Field
 */
 
 var myMainDiv = document.getElementById("main");
 console.log(myMainDiv.childNodes[1].nextSibling);
 console.log(myMainDiv.childNodes[1].nextElementSibling);
 console.log(myMainDiv.childNodes[2].previousSibling.textContent = 'This Text Was Changed By JavaScript');
+
+window.onload = function () {
+    document.getElementById('myInput').focus();
+}
+document.querySelector('button').onclick = function () {
+    document.getElementById('myInput').blur();
+}
