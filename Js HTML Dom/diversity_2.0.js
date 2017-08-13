64ee61cd4dd153a519e4b6c66252e4868f24bf71
Hwiki09 <oubaye.rouaki@gmail.com>
2017-08-13 18:42:18 +0200

    Style

---
+++ Js HTML Dom/diversity_2.0.js
@@ -0,0 +1,17 @@
+/*
+    Style:
+        Element.style.property = 'value'
+
+
+*/
+
+// Style
+var mainDiv = document.getElementById("main");
+mainDiv.style.backgroundColor = 'red';
+mainDiv.style.padding = '10px';
+mainDiv.style.color = 'Black';
+mainDiv.style.fontFamily = 'Consolas';
+
+
+
+
