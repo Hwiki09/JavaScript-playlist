45617ae1ce9667e266a905d461b25e1879c0f4a4
Hwiki09 <oubaye.rouaki@gmail.com>
2017-08-13 18:54:54 +0200

    Create: Element, textNode, Comment

--- Js HTML Dom/diversity_2.0.js
+++ Js HTML Dom/diversity_2.0.js
@@ -2,6 +2,10 @@
     Style:
         Element.style.property = 'value'
 
+    Create: 
+        Element
+        textNode
+        Comment
 */
 
 // Style
@@ -18,5 +22,11 @@ console.log(document.lastModified);
 console.log(document.URL);
 
 // Create: Element, textNode, Comment
-
-
+var my1stComment = document.createComment('This is The 1st JavaScript Comment'); // Create a Comment
+document.body.appendChild(my1stComment); // Append to Body
+var myElement = document.createElement('p'); // Create a Paragraph Element
+document.body.appendChild(myElement); // Append it to Body
+var myText = document.createTextNode('Im a Text'); // Create a Text
+myElement.appendChild(myText);  // When The Variable is Created -> No more need to write "document" befor the element !! // Append it to P
+var my2ndComment = document.createComment('This is The 2nd JavaScript Comment'); // Create a Comment
+document.body.appendChild(my2ndComment); // Append to Body
